2bc7c4f8d24be813595963d69274f9ee339098cf
Yannis Zarkadas <yanniszarkadas@gmail.com>
2017-05-22 16:25:53 +0300

    Menu buttons finalized.

--- app/src/main/java/com/example/android/musicplayer/RadioActivity.java
+++ app/src/main/java/com/example/android/musicplayer/RadioActivity.java
@@ -14,21 +14,37 @@ public class RadioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_radio);
     }
 
+    /**
+     * Navigate to trending stations activity.
+     * @param view
+     */
     public void showTrendingStations(View view){
         Intent i = new Intent(this, TrendingRadioActivity.class);
         startActivity(i);
     }
 
+    /**
+     * Navigate to favorite stations activity.
+     * @param view
+     */
     public void showFavoriteStations(View view){
         Intent i = new Intent(this, FavoriteRadioActivity.class);
         startActivity(i);
     }
 
+    /**
+     * Navigate to local stations activity.
+     * @param view
+     */
     public void showLocalStations(View view){
         Intent i = new Intent(this, LocalRadioActivity.class);
         startActivity(i);
     }
 
+    /**
+     * Navigate to global stations activity.
+     * @param view
+     */
     public void showGlobalStations(View view){
         Intent i = new Intent(this, GlobalRadioActivity.class);
         startActivity(i);
